0c918d36bfb28672cf81d30e6457f0071f4ddd71
Urvashi Chhabra <urvashi.chhabra@spenda.co>
2024-09-26 11:04:32 +0530

    ES6 web server

--- server.js
+++ server.js
@@ -1,14 +1,45 @@
 import http from "http";
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
 
-const PORT = 8000;
+const PORT = process.env.PORT;
 
-const server = http.createServer((req, resp) => {
-  //   resp.setHeader("Content-Type", "text/plain");
-  //   resp.write("<h1>HELLO WORLD!</h1>");
-  //   resp.end();
+const fileName = url.fileURLToPath(import.meta.url);
+const dirName = path.dirname(fileName);
 
-  resp.writeHead(500, { "Content-Type": "application/json" });
-  resp.end(JSON.stringify({ message: "SERVER ERROR" }));
+const server = http.createServer(async (req, resp) => {
+  try {
+    if (req.method === "GET") {
+      let filePath;
+      // if (req.url === "/") {
+      //   resp.writeHead(200, { "Content-Type": "text/html" });
+      //   resp.end("<h1>HomePage</h1>");
+      // } else if (req.url === "/about") {
+      //   resp.writeHead(200, { "Content-Type": "text/html" });
+      //   resp.end("<h1>About</h1>");
+      // } else {
+      //   resp.writeHead(404, { "Content-Type": "text/html" });
+      //   resp.end("<h1>Not Found</h1>");
+      // }
+      if (req.url === "/")
+        filePath = path.join(dirName, "public", "index.html");
+      else if (req.url === "/about")
+        filePath = path.join(dirName, "public", "about.html");
+      else throw new Error("Not Found");
+
+      const data = await fs.readFile(filePath);
+
+      resp.setHeader("Content-Type", "text/html");
+      resp.write(data);
+      resp.end();
+    } else {
+      throw new Error("Method not allowed.");
+    }
+  } catch (e) {
+    resp.writeHead(500, { "Content-Type": "text/plain" });
+    resp.end("Server Error");
+  }
 });
 
 server.listen(PORT, () => {
